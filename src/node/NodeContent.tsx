aacffac9e19e6c67b590cf2c5d53548b7826a871
pietro rosa <pietrorosa@microsoft.com>
2023-02-28 09:57:29 +0100

    fixed loading

--- src/node/NodeContent.tsx
+++ src/node/NodeContent.tsx
@@ -1,40 +1,50 @@
-import { createEffect, createSignal } from "solid-js";
+import { createSignal, onMount, Show } from "solid-js";
 import styles from "./Node.module.css";
 
+const getNodeContent = async (
+  md: string,
+  separator: string,
+  transpiler: (input: string) => Promise<string>
+) => {
+  try {
+    const parts = md.split(separator);
+    const promises = parts.map((p) => transpiler(p));
+    const partsParsed = await Promise.all(promises);
+    const nodeHtmBloksHtml = partsParsed.map(
+      (p) =>
+        `<div class="${styles.NodeContentPart}"><div class="dumbot-content-body">${p}</div></div>`
+    );
+    return nodeHtmBloksHtml.join("");
+  } catch (error) {
+    console.error(error);
+    return "<strong>Error converting html</strong>";
+  }
+};
 export const NodeContentReadonly = (props: {
   content: any;
   separator: string;
   getHtmlContent: (content: string) => Promise<string>;
 }) => {
-  const [nodeContent, setNodeContent] = createSignal<string>();
-  const getNodeContent = async (md: string) => {
-    try {
-      const parts = md.split(props.separator);
-      const promises = parts.map((p) => props.getHtmlContent(p));
-      const partsParsed = await Promise.all(promises);
-      const nodeHtmBloksHtml = partsParsed.map(
-        (p) =>
-          `<div class="${styles.NodeContentPart}"><div class="dumbot-content-body">${p}</div></div>`
-      );
-      return nodeHtmBloksHtml.join("");
-    } catch (error) {
-      console.error(error);
-      return "<strong>Error converting html</strong>";
-    }
-  };
+  const [nodeContent, setNodeContent] = createSignal<string | undefined>(
+    undefined
+  );
 
-  createEffect(() => {
-    getNodeContent(props.content).then((newContent) => {
-      setNodeContent(newContent);
-    });
-    // eslint-disable-next-line
-  }, [props.content]);
+  onMount(async () => {
+    const html = await getNodeContent(
+      props.content,
+      props.separator,
+      props.getHtmlContent
+    );
+    setNodeContent(html);
+  });
 
   return (
-    <div
-      // eslint-disable-next-line
-      innerHTML={nodeContent ? nodeContent() : "loading markdown..."}
-      style={{ width: "100%", cursor: "unset" }}
-    />
+    <Show when={nodeContent()} fallback={<div>loading....</div>}>
+      <div
+        // eslint-disable-next-line
+        innerHTML={nodeContent()} // sanitize the content is responsibility of the transpiler
+        style={{ width: "100%", cursor: "unset" }}
+      />
+    </Show>
   );
 };
